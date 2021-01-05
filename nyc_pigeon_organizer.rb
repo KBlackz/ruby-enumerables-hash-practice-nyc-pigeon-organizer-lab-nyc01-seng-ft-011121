3751477a6a82e9f4574298edbe36dfa9186c4e25
Karris  Black <karris.b@gmail.com>
2021-01-05 09:46:55 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,6 +1,6 @@
 def nyc_pigeon_organizer(data)
   final_result = data.each_with_object({}) do |(key, value), final_array|
-    value.each do |inner_key, name|
+    value.each do |inner_key, names|
       names.each do ||names|
         if ! final_array[name]
           final_array[name] = {}
